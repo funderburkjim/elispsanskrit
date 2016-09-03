b7f0b0763cbe61ceb54495e5578784e34650e122
funderburkjim <funderburkjim@gmail.com>
2016-09-03 19:01:00 -0400

    refine update from verbdata

--- pysanskritv1/dhaval/function/redo.sh
+++ pysanskritv1/dhaval/function/redo.sh
@@ -2,6 +2,10 @@ echo "copying function.php"
 cp ~/Documents/GitHub/SanskritVerb/scripts/function.php .
 echo "extracting verbdata"
 php extract.php verbdata
+echo "extracting AkusmIya"
+php extract.php AkusmIya
+echo "extracting AgarvIya"
+php extract.php AgarvIya
 echo "checking AkusmIya, AgarvIya v. verbdata"
 python check_class10_pada.py verbdata.txt AkusmIya.txt AgarvIya.txt > check_class10_pada_log.txt
 echo "examine check_class10_pada_log.txt"
